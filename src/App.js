a39afcf1d52fee2807e35ff01c1c12b86474cb12
zohaibmuhammadawan <prinxzohaib@gmail.com>
2022-01-07 01:28:39 +0500

    basic file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
